0b7e57bf8fed1d6cf5ba76488cb589d6d5df3019
kintoyyy <56209108+Kintoyyy@users.noreply.github.com>
2024-06-02 01:02:24 +0800

    added JuanFi Rates Changer Scheduler

---
+++ pages/js/menu.js
@@ -0,0 +1,27 @@
+function createMenu(menuItems) {
+    const menu = document.getElementById('menu');
+    menuItems.forEach(item => {
+        if (item.type === 'link') {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = item.href;
+            a.textContent = item.text;
+            li.appendChild(a);
+            menu.appendChild(li);
+        } else if (item.type === 'divider') {
+            const div = document.createElement('div');
+            div.className = 'divider divider-secondary';
+            div.textContent = item.text;
+            menu.appendChild(div);
+        }
+    });
+}
+
+fetch('/pages/js/menuData.json')
+    .then(response => response.json())
+    .then(data => {
+        createMenu(data.menuItems);
+    })
+    .catch(error => {
+        console.error('Error fetching the JSON data:', error);
+    });
